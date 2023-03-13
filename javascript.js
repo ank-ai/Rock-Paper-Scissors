f6c6e5786f4bcbe9e7d97ed2fa1849fb4f9d30fa
Ankai Yan <ankai.yan@icloud.com>
2023-03-13 22:44:43 +0100

    Implement Javascript DOM
    I added the Score display,
    buttons to choose from R/P/C and
    a winner message will be announced,
    next push will clean up code, fix bugs and
    make UI prettier

--- javascript.js
+++ javascript.js
@@ -16,13 +16,13 @@ function playRound(playerChoice, computerChoice) {
     if (playerChoice === "ROCK") {
         switch (computerChoice) {
             case "PAPER":
-                console.log(`Computer chose: ${computerChoice}. ${computerChoice} beats ${playerChoice}. You Lose!`)
+
                 return -1
             case "SCISSOR":
-                console.log(`Computer chose: ${computerChoice}. ${playerChoice} beats ${computerChoice}. You Win!`)
+
                 return 1
             default:
-                console.log(`Computer chose: ${computerChoice}. It's a Tie!`)
+
                 return 0
         }
     }
@@ -30,13 +30,13 @@ function playRound(playerChoice, computerChoice) {
     if (playerChoice === "PAPER") {
         switch (computerChoice) {
             case "SCISSOR":
-                console.log(`Computer chose: ${computerChoice}. ${computerChoice} beats ${playerChoice}. You Lose!`)
+
                 return -1
             case "ROCK":
-                console.log(`Computer chose: ${computerChoice}. ${playerChoice} beats ${computerChoice}. You Win!`)
+
                 return 1
             default:
-                console.log(`Computer chose: ${computerChoice}. It's a Tie!`)
+
                 return 0
         }
     }
@@ -44,36 +44,118 @@ function playRound(playerChoice, computerChoice) {
     if (playerChoice === "SCISSOR") {
         switch (computerChoice) {
             case "ROCK":
-                console.log(`Computer chose: ${computerChoice}. ${computerChoice} beats ${playerChoice}. You Lose!`)
+
                 return -1
             case "PAPER":
-                console.log(`Computer chose: ${computerChoice}. ${playerChoice} beats ${computerChoice}. You Win!`)
+
                 return 1
             default:
-                console.log(`Computer chose: ${computerChoice}. It's a Tie!`)
                 return 0
         }
     }
 }
 
-function game(){
 
-    let playerScore = 0;
-    let computerScore = 0;
 
-    for(let i=1; i<6; i++){
-        let playerChoice = prompt(`Round ${i}. Choose your weapon`);
-        let winner =  playRound(playerChoice,getComputerChoice());
-        switch (winner){
-            case -1: computerScore++;
-            break;
-            case 1: playerScore++;
-            break;
-            default:
-        }
-        console.log(`Current Score: You've got ${playerScore}, Computer has ${computerScore}.`)
+const rockButton = document.createElement("button");
+const paperButton = document.createElement("button");
+const scissorButton = document.createElement("button");
+rockButton.textContent = "ROCK";
+paperButton.textContent = "PAPER";
+scissorButton.textContent = "SCISSOR";
+const bod = document.querySelector("body");
+bod.appendChild(rockButton);
+bod.appendChild(paperButton);
+bod.appendChild(scissorButton);
+
+
+function doSth() {
+    alert("I'm doing something :)");
+}
+
+function doAnotherThing(number) {
+    alert(number);
+}
+
+function runRock() {
+    const playerChoice = "ROCK";
+    const computerChoice = getComputerChoice()
+    resultNumber = playRound(playerChoice, computerChoice);
+    result.textContent = evaluateNumber(playerChoice, computerChoice, resultNumber);
+    scoreDisplay.textContent = updateScore();
+}
+
+function runPaper() {
+    const playerChoice = "PAPER"
+    const computerChoice = getComputerChoice();
+    resultNumber = playRound(playerChoice, computerChoice);
+    result.textContent = evaluateNumber(playerChoice, computerChoice, resultNumber);
+    scoreDisplay.textContent = updateScore();
+}
+
+function runScissor() {
+    const playerChoice = "SCISSOR";
+    const computerChoice = getComputerChoice()
+    resultNumber = playRound(playerChoice, computerChoice);
+    result.textContent = evaluateNumber(playerChoice, computerChoice, resultNumber);
+    scoreDisplay.textContent = updateScore();
+}
+
+
+
+rockButton.addEventListener('click', runRock);
+paperButton.addEventListener("click", runPaper);
+scissorButton.addEventListener("click", runScissor);
+
+const result = document.querySelector(".result");
+
+const scoreDisplay = document.createElement('div');
+
+bod.appendChild(scoreDisplay);
+
+function evaluateNumber(playerChoice, computerChoice, number) {
+
+    
+    if (number === 0) {
+        return `You chose: ${playerChoice}, Computer chose: ${computerChoice}.  It's a Tie!`
+    } else if (number === 1) {
+        playerScore++;
+
+        if(playerScore===5){ return "Player Wins!"}
+
+        return `You chose: ${playerChoice}, Computer chose: ${computerChoice}.  You Win!`
+    } else {
+
+        computerScore++;
+        if(computerScore===5){   return "Computer Wins!" }
+     
+        return `You chose: ${playerChoice}, Computer chose: ${computerChoice}.  You Lose!`
     }
 }
 
-game();
+function updateScore() {
+    if((playerScore <5) || (computerScore <5)){ return `Player score: ${playerScore}  Computer score: ${computerScore}.`}
+    //otherwise
+
+  //  if(playerScore>5){
+     //   result.textContent="Player Wins!"
+   //     return`Final score: ${playerScore} to ${computerScore}.`
+
+   //  }else{
+
+   //     result.textContent="Computer Wins!"
+   //     return`Final score: ${computerScore} to ${playerScore} .`
+   //  }
+    
+
+}
+
+
+let playerScore = 0;
+let computerScore = 0;
+
+//display round
+
+
+
 
